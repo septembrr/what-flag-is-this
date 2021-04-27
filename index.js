506c6526a345362ff7b08a9342c33f77de3c26e3
septembrr <43982603+septembrr@users.noreply.github.com>
2021-04-27 16:19:34 -0700

    Set up express, handlebars and base routes.

---
+++ index.js
@@ -0,0 +1,44 @@
+// Express Routes
+var express = require('express');
+var app = express();
+
+// Body parser for POST endpoint routes
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Handlebars Template Engine
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 8080);
+
+app.use(express.static('public'));
+
+// Image Service
+app.post('/image',function(req,res,next){
+    return res.send("Image Result");
+});
+
+// Main Page
+app.get('/',function(req,res){
+    res.render('index');
+});
+
+// 404
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+// 500
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started; press Ctrl-C to terminate.');
+});
