82f7c2d4fabd4b552b2e7f10c7ce7b8b70a96ca1
septembrr <43982603+septembrr@users.noreply.github.com>
2021-05-22 21:45:54 -0700

    Add error handling when image scraper may be down.

--- index.js
+++ index.js
@@ -124,6 +124,7 @@ app.get('/', function(req, res){
         res.render('index', context);
     }).catch(function(err){
         console.log("ERROR", err);
+        res.render('error');
     });
 });
 
